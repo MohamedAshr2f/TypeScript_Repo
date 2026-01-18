4298692c98080a24225a965027116fb77631ff69
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-01-18 03:31:03 +0200

    add arrays

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,6 @@ function add(num1: number, num2: number)
     return num1 + num2;
 }
 console.log(add(5,10));
+////////////////////
+let list1: number[]=[1,2,3,4,5];
+let list2=["mohamed","ashraf","ali",10];
