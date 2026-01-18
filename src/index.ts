ea0aa55ae39045f5a56ee46e0c707fc3fafbd9ed
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-01-18 04:57:24 +0200

    add enums

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,24 @@ console.log(add(5,10));
 ////////////////////
 let list1: number[]=[1,2,3,4,5];
 let list2=["mohamed","ashraf","ali",10];
+
+function display(...args: number[])
+{
+    for(let i=0;i<args.length;i++)
+    {
+        console.log(args[i]);
+    }
+    args.forEach(element => console.log(element));
+}
+
+enum level{
+    kids=8, 
+    beginner=5,
+    advanced=2,
+    expert=1
+}
+let mylevel: string="beginner";
+if(mylevel=="beginner")
+    {
+        console.log(level.beginner);
+   }
