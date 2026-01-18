fa963119a616b5081786cd2c80e30cb4e018f875
Mohamed Ashraf <mohamedashraf20191700504@gmail.com>
2026-01-18 03:15:56 +0200

    add functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log(Math.floor(10.5));
+let name = "mohamed";
+let age = 30;
+let isStudent = true;
+let all : any="mhamedashraf";
+all=30;
+
+function add(num1: number, num2: number)
+{
+    return num1 + num2;
+}
+console.log(add(5,10));
